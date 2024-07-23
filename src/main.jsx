e43d8b5244e47c4bc84e63a85eb774e8aa62c730
Samolyb <artemte6575@gmail.com>
2024-07-23 21:41:22 +0100

    update-components

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from './redux/store'
-import App from "./Components/App/App"
+import App from "./components/App/App"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
